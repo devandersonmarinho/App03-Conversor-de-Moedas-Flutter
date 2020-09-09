e17b84eb7cf97c2db8170c9652ded6bd94f1e92d
hao <haokidysss@gmail.com>
2020-09-09 19:59:01 -0400

    Import html, dando um get na Api, exibindo o resultado no terminal - import em  async para exibir o resultado sem carregar a pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'key.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
 
 const request = key;
 
-void main() {
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(response.body);
+
   runApp(MaterialApp(
     home: Container(),
   ));
