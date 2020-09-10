1add6f9a61d5ebbbfde9936837293a1006e766c3
hao <haokidysss@gmail.com>
2020-09-09 21:57:39 -0400

    Criando as variaveis do dolar e euro com as informaçoes filtradas da Api e Criando a interface do App (A correçao anterior foi para que a borda adicionada aqui funcione, foi configurado de maneira global)

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,8 @@ void main() async {
           focusedBorder:
               OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
           hintStyle: TextStyle(color: Colors.amber),
-        )),
+        )
+    ),
   ));
 }
 
@@ -33,6 +34,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,8 +65,55 @@ class _HomeState extends State<Home> {
                         textAlign: TextAlign.center),
                   );
                 } else {
-                  return Container(
-                    color: Colors.green,
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding:  EdgeInsets.all(10.0) ,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Colors.amber,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$",
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Dólares",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "US\$",
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Euros",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "€",
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber
+                          ),
+                        ),
+                      ],
+                    ),
                   );
                 }
             }
