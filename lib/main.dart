5c5f639236ef44695509212fc3c3cbb112af97aa
hao <haokidysss@gmail.com>
2020-09-09 20:01:13 -0400

    import na biblioteca convert do dart para converter o resultado do get em um json e e filtrando os resultados com as chaves[]

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'key.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
-
+import 'dart:convert';
 
 const request = key;
 
 void main() async {
 
   http.Response response = await http.get(request);
-  print(response.body);
+  print(json.decode(response.body)['results']['currencies']['USD']);
 
   runApp(MaterialApp(
     home: Container(),
