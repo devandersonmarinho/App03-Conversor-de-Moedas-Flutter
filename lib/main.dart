fc433fa528326987aa885ee4e7544dcf35dc3475
hao <haokidysss@gmail.com>
2020-09-09 20:11:57 -0400

    Transformando a request em uma funçao getData()

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'key.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = key;
 
 void main() async {
-
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
+  
+  print(await getData());
 
   runApp(MaterialApp(
     home: Container(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+
 }
